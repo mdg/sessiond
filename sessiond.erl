68c56808642af45f6ca4e6e027c413300c16d1f2
Matthew Graham <mdg149@optonline.net>
2009-10-31 06:33:39 -0400

    changed sessiond to publish session store
    this lets it be accessible from yaws pages where the store
    can't be passed into directly

--- sessiond.erl
+++ sessiond.erl
@@ -1,13 +1,14 @@
 -module(sessiond).
--export([start/0, serve/3]).
+-export([start/0, serve/2]).
 
 
 % {session, SessionID, UserID, Expiration}
 
 start() ->
-	{spawn(fun server_loop/0), ets:new(session, [set])}.
+	spawn(fun run_server/0).
 
-serve({Server, Store}, Command, Param) ->
+serve(Command, Param) ->
+	{Server, Store} = find_server(),
 	Server ! {self(), Store, {Command, Param}},
 	receive
 		{Server, Result} ->
@@ -27,6 +28,27 @@ renew_session(SessionID) ->
 kill_session(SessionID) ->
 	SessionID.
 
+
+
+run_server() ->
+	publish_server(),
+	server_loop(),
+	close_server().
+
+publish_server() ->
+	SessionStore = ets:new(session, [set]),
+	Server = {self(), SessionStore},
+	ServerStore = ets:new(server, [set, named_table]),
+	ets:insert(ServerStore, {server, Server}).
+
+find_server() ->
+	[{server, Server}] = ets:lookup(server, server),
+	Server.
+
+close_server() ->
+	{_ServerProc, SessionStore} = find_server(),
+	ets:delete(SessionStore),
+	ets:delete(server).
 
 server_loop() ->
 	receive
