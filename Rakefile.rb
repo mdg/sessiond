51f4ab4d164e28f00ffc0d6a50f624b8541ac756
Matthew Graham <mdg149@gmail.com>
2010-08-23 00:20:40 -0400

    moved sessiond.erl into src/
    updated the Rakefile to work with new path.
    also fixed the Rakefile to correctly launch the app.

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,18 +1,18 @@
 
 
-file 'sessiond.beam' => ['sessiond.erl'] do |t|
-	sh('erlc sessiond.erl')
+file 'ebin/sessiond.beam' => ['src/sessiond.erl'] do |t|
+	sh('erlc -o ebin/ src/sessiond.erl')
 end
 
-task :app => ['sessiond.beam']
+task :app => ['ebin/sessiond.beam']
 
 task :default => ['app']
 
 task :devrun => ['app'] do |t|
-	sh('erl -pa mochiweb/deps/mochiweb-src/ebin/ -s reloader -s sessiond start')
+	sh('erl -pa ebin deps/mochiweb deps/rabbitmq-erlang-client deps/rabbitmq-server -s sessiond start')
 end
 
 task :run => ['app'] do |t|
-	sh('erl -noshell -s sessiond start -s init stop')
+	sh('erl -pa ebin deps/mochiweb deps/rabbitmq-erlang-client deps/rabbitmq-server -noshell -s sessiond start')
 end
 
